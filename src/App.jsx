25682c652ef36fc54fe8f8441bf37baedd5914dd
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-12-11 17:14:02 +0000

    Move boardbuilder constructor to scripts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.scss'
 // import { Test } from './components/Test'
 import { Board } from './components/Board'
 import { testBoard } from './data'
+import { boardBuild } from './scripts/boardBuilder'
 
 function App() {
+  const newBoard = boardBuild(testBoard)
 
   return (
     <>
-      <Board board={testBoard} />
+      <Board board={newBoard} />
     </>
   )
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,41 +14,9 @@ function BoardRow({ row }) {
 }
 
 export function Board({ board }) {
-  function Tile(letter, row, col, willContainLetter) { //tile constuctor
-    return {
-      letter: letter,
-      row: row,
-      col: col,
-      willContainLetter: willContainLetter,
-      active: false
-    }
-  }
-  function boardBuild(board) { //this function converts each letter in the board into an object
-    const newBoard = []
-    for (let i = 0; i < board.solution.length; i++) {
-      const newRow = []
-      for (let j = 0; j < board.solution[i].length; j++) { //buildrow
-        let willContainLetter = false
-        if (board.solution[i][j]) {
-          willContainLetter = true
-        }
-        newRow.push(
-          new Tile(
-            board.start[i][j],
-            i,
-            j,
-            willContainLetter
-          )
-        )
-      }
-      newBoard.push(newRow)
-    }
-    return newBoard
-  }
-  const newBoard = boardBuild(board)
   return (
     <div className="board">{
-      newBoard.map((row, index) => (
+      board.map((row, index) => (
         <BoardRow row={row} key={index} />
       ))
     }
